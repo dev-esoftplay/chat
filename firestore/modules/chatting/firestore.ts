e3908a6dddaa6159cc3f5c27c92af9bc77431d3a
mukhlis <mukhlis@fisip.net>
2022-09-19 17:15:52 +0700

    add firestore function

--- firestore/modules/chatting/firestore.ts
+++ firestore/modules/chatting/firestore.ts
@@ -3,7 +3,7 @@
 
 import _global from 'esoftplay/_global';
 import { initializeApp } from 'firebase/app';
-import { collection, doc, FieldPath, getDoc, getDocs, getFirestore, query, where, WhereFilterOp } from 'firebase/firestore/lite';
+import { addDoc, collection, deleteDoc, doc, FieldPath, getDoc, getDocs, initializeFirestore, onSnapshot, query, setDoc, updateDoc, where, WhereFilterOp } from 'firebase/firestore';
 
 export interface DataId {
   id: string,
@@ -15,29 +15,59 @@ export type id = string
 const Firestore = {
   init() {
     _global.firebaseApp = initializeApp({
-      "apiKey": "AIzaSyB04JT4JJfFsArIccAjBEn1nwIlg8EVWx4",
-      "authDomain": "bigbang-online.firebaseapp.com",
-      "databaseURL": "https://bigbang-online.firebaseio.com/",
-      "storageBucket": "gs://bigbang-online.appspot.com/",
-      "projectId": "bigbang-online"
+      apiKey: "AIzaSyChqxbhmf7Qk_CagMc6v_bPeegXcLNkUqE",
+      authDomain: "esoftplay-log.firebaseapp.com",
+      databaseURL: "https://esoftplay-log-default-rtdb.firebaseio.com",
+      projectId: "esoftplay-log",
+      storageBucket: "esoftplay-log.appspot.com",
+      messagingSenderId: "844016531377",
+      appId: "1:844016531377:web:892688387299a7b0d3af90"
     })
+    // _global.firebaseApp = initializeApp({
+    //   "apiKey": "AIzaSyB04JT4JJfFsArIccAjBEn1nwIlg8EVWx4",
+    //   "authDomain": "bigbang-online.firebaseapp.com",
+    //   "databaseURL": "https://bigbang-online.firebaseio.com/",
+    //   "storageBucket": "gs://bigbang-online.appspot.com/",
+    //   "projectId": "bigbang-online"
+    // })
   },
   db() {
     if (!_global.firebaseFirestore)
-      _global.firebaseFirestore = getFirestore(_global.firebaseApp)
+      // _global.firebaseFirestore = getFirestore(_global.firebaseApp)
+      _global.firebaseFirestore = initializeFirestore(_global.firebaseApp, {
+        experimentalForceLongPolling: true
+      })
     return _global.firebaseFirestore
   },
   add: {
-    doc() {
-
+    doc(path: string[], value: any, cb: () => void, err?: (error: any) => void) {
+      if (path.length % 2 > 0) {
+        console.warn("path untuk akses Doc data tidak boleh berhenti di Collection")
+        return
+      }
+      const colRef = doc(Firestore.db(), ...path)
+      setDoc(colRef, value).then((snap) => {
+        cb()
+      }).catch(err)
     },
-    collection() {
-
+    collection(path: string[], value: any, cb: (dt: any) => void, err?: (error: any) => void) {
+      if (path.length % 2 == 0) {
+        console.warn("path untuk akses Collection data tidak boleh berhenti di Doc")
+        return
+      }
+      //@ts-ignore
+      const colRef = collection(Firestore.db(), ...path)
+      addDoc(colRef, value).then((snap) => {
+        cb({ id: snap?.id })
+      }).catch(err)
     }
   },
-  delete:{
-    doc(){
-      
+  delete: {
+    doc(path: string[], cb: () => void, err?: (error: any) => void) {
+      const colRef = doc(Firestore.db(), ...path)
+      deleteDoc(colRef).then((snap) => {
+        cb()
+      }).catch(err)
     }
   },
   get: {
@@ -90,19 +120,42 @@ const Firestore = {
     }
   },
   listen: {
-    collection() {
-
+    collection(path: string[], cb: (dt: any) => void, err?: (error: any) => void): () => void {
+      // @ts-ignore
+      const colRef = collection(Firestore.db(), ...path)
+      let datas: any[] = []
+      const unsub = onSnapshot(colRef, (snap) => {
+        datas = []
+        snap.docs.forEach((doc) => {
+          datas.push({ data: doc.data(), id: doc.id })
+        })
+        cb(datas)
+      }, err)
+      return () => unsub()
     },
-    doc() {
-
+    doc(path: string[], cb: (dt: any) => void, err?: (error: any) => void): () => void {
+      // @ts-ignore
+      const colRef = doc(Firestore.db(), ...path)
+      const unsub = onSnapshot(colRef, (snap) => {
+        cb(snap.data())
+      }, err)
+      return () => unsub()
     }
   },
   update: {
     collection() {
 
     },
-    doc() {
+    doc(path: string[], value: string, cb: () => void, err?: (error: any) => void) {
+      let nPath = [path[0], path[1]]
+      let par = path.filter((t, i) => i != 0 && i != 1)
 
+      const colRef = doc(Firestore.db(), ...nPath)
+      updateDoc(colRef, {
+        [`${par.join(".")}`]: value
+      }).then((e) => {
+        cb()
+      }).catch(err)
     }
   },
   query() {
--- firestore/modules/chatting/main.tsx
+++ firestore/modules/chatting/main.tsx
@@ -1,11 +1,14 @@
 // withHooks
 
 import { useSafeState } from 'esoftplay';
+import { LibIcon } from 'esoftplay/cache/lib/icon/import';
+import { LibInput } from 'esoftplay/cache/lib/input/import';
 import { LibList } from 'esoftplay/cache/lib/list/import';
 import { LibStyle } from 'esoftplay/cache/lib/style/import';
 import { LibTextstyle } from 'esoftplay/cache/lib/textstyle/import';
-import React, { useEffect } from 'react';
-import { View } from 'react-native';
+import useGlobalState from 'esoftplay/global';
+import React, { useEffect, useRef } from 'react';
+import { Pressable, View } from 'react-native';
 import Firestore from './firestore';
 
 
@@ -15,30 +18,96 @@ export interface ChattingMainArgs {
 export interface ChattingMainProps {
 
 }
+const input = useGlobalState<string>('')
 export default function m(props: ChattingMainProps): any {
   const [list, setList] = useSafeState<any[]>([]);
-
+  const inputRef = useRef<LibInput>(null)
 
   useEffect(() => {
     Firestore.init()
     Firestore.db()
-    Firestore.get.collectionIds(['users'], [], (list) => {
-      setList(list)
+    // Firestore.get.collection(['users'], [], (list) => {
+    //   setList(list)
+    // })
+    // Firestore.get.doc(['users', 'TB87yOhF8cH81gVmcQjA', 'chat_ids', 'UTYHdksUqjf3gFdVFrRq'], [], (doc) => {
+    //   console.log("DOC", doc)
+    // })
+    Firestore.listen.collection(['users'], (data) => {
+      setList(data)
+    }, (err) => {
+      console.log(err);
     })
-    Firestore.get.doc(['users', 'TB87yOhF8cH81gVmcQjA', 'chat_ids','UTYHdksUqjf3gFdVFrRq'], [], (doc) => {
-      console.log("DOC", doc)
-    })
+    // Firestore.listen.doc(['users', 'gae58AMVAaKaygT0KppS'], (data) => {
+    //   // setList(data)
+    //   console.log(data);
+    // }, (err) => {
+    //   console.log(err);
+    // })
   }, [])
 
 
   return (
     <View style={{ flex: 1, paddingTop: LibStyle.STATUSBAR_HEIGHT + 10 }} >
+
+      <Pressable onPress={() => {
+        Firestore.add.doc(['users', 'gae58AMVAaKaygT0KppS', 'chat', 'wu3O7SqjhjWJKU4q3maI'], { id: 1, name: 'mukhlis' }, () => {
+          console.log('sukses add doc');
+        })
+      }} style={{ marginBottom: 10, alignItems: 'center', padding: 10, backgroundColor: 'orange' }}>
+        <LibTextstyle text='ADD DOC' textStyle='headline' style={{ color: 'white' }} />
+      </Pressable>
+
+      <Pressable onPress={() => {
+        Firestore.add.collection(['users'], { id: 1, name: 'mukhlis' }, () => {
+          console.log('sukses add collection');
+        }, (e) => {
+          console.log(e);
+        })
+      }} style={{ marginBottom: 10, alignItems: 'center', padding: 10, backgroundColor: 'orange' }}>
+        <LibTextstyle text='ADD COLLECTION' textStyle='headline' style={{ color: 'white' }} />
+      </Pressable>
+
       <LibList
         data={list}
-        renderItem={(item) => {
+        renderItem={(item: any, i: number) => {
           return (
-            <View style={{ height: 100 }} >
-              <LibTextstyle text={JSON.stringify(item)} textStyle="body" />
+            <View key={Number(item?.data?.id)} style={{ marginBottom: 20, marginRight: 20 }} >
+              <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+                <LibTextstyle text={JSON.stringify(item)} textStyle="body" style={{ flex: 1 }} />
+                {
+                  item.id != "gae58AMVAaKaygT0KppS" &&
+                  <Pressable onPress={() => {
+                    Firestore.delete.doc(['users', item.id], () => {
+                      console.log('deleted');
+
+                    }, (err) => { })
+                  }} >
+                    <LibIcon name="delete" />
+                  </Pressable>
+                }
+              </View>
+              <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+                <View style={{ borderWidth: 1, flex: 1, marginRight: 10 }}>
+                  <LibInput
+                    ref={inputRef}
+                    base
+                    defaultValue={item?.data?.name}
+                    onChangeText={(t) => {
+                      input.set(t)
+                    }}
+                    style={{ flex: 1, padding: 10 }}
+                  />
+                </View>
+                <Pressable onPress={() => {
+                  Firestore.update.doc(['users', item.id, 'name'], input.get(), () => {
+
+                  }, (err) => {
+
+                  })
+                }}>
+                  <LibIcon name="pencil" />
+                </Pressable>
+              </View>
             </View>
           )
         }}
