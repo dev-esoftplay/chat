0a3d3316f9c23744b4df276d2be5a4e392d30126
mukhlis <mukhlis@fisip.net>
2024-10-23 09:32:31 +0700

    update cache to mmkv

--- mobile/modules/chatting/cache.ts
+++ mobile/modules/chatting/cache.ts
@@ -1,28 +1,34 @@
 // useLibs
-// noPage
 
-import useSafeState from "esoftplay/state";
-import Storage from "esoftplay/storage";
-import { useLayoutEffect } from "react";
+import { UserData } from 'esoftplay/cache/user/data/import';
+import FastStorage from 'esoftplay/mmkv';
+import useSafeState from 'esoftplay/state';
+import { useLayoutEffect } from 'react';
 
-
-export default function m(initValue: any, persistKey: string) {
-  const [a, b] = useSafeState<any>([])
+export default function m<T>(initValue: T, persistKey: string) {
+  UserData.register(persistKey)
+  const [state, setState, get] = useSafeState<T>(initValue);
 
   useLayoutEffect(() => {
-    get()
-  }, [])
+    const storedValue: any = FastStorage.getItemSync(persistKey);
+    try {
+      if (storedValue !== null && storedValue != undefined) {
+        setState(JSON.parse(storedValue));
+      }
+    } catch (error) {
+      console.error("Failed to parse stored value:", error);
+      setState(initValue);
+    }
+  }, [persistKey]);
 
-  function get() {
-    Storage.getItem(persistKey).then((val) => {
-      b(val != null ? val : initValue)
-    })
-  }
+  const setPersistentState = (value: T) => {
+    setState(value);
+    try {
+      FastStorage.setItem(persistKey, JSON.stringify(value));
+    } catch (error) {
+      console.error("Failed to store value:", error);
+    }
+  };
 
-  function set(val: any) {
-    b(val)
-    Storage.setItem(persistKey, JSON.stringify(val))
-  }
-
-  return [a, set]
+  return [state, setPersistentState, get] as const;
 }
